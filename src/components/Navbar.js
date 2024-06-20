f850fc9566a86a34946ad24b2f82724758ecce6b
NHammond <natehammond62@gmail.com>
2024-06-20 11:49:36 -0400

    site title is actually clickable!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom"
 export default function Navbar(){
     return(
             <nav id="navbar">
-                <h1 id="site-title">Infinite Horizons</h1>
+                <h1 href="#" id="site-title">Infinite Horizons</h1>
                 <div id="navlinks">
                     <Link to="/">Home</Link>
                     <Link to="/Dojo">Dojo</Link>
