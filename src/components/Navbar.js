f52a0de2b18924c61cb2891e84e9f816ba3fe8b5
NHammond <natehammond62@gmail.com>
2024-06-20 21:45:48 -0400

    css and jsx updated for almost completely responsive navbar, and static images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"
 
 
 
-
 export default function Navbar(){
+const [menuOpen, setMenuOpen] = useState(false);
+
+
     return(
             <nav id="navbar">
+                
                 <h1 href="#" id="site-title"><a href="#">Infinite Horizons</a></h1>
-                <div id="navlinks">
+                
+                    
+                {menuOpen && (
+                <div id="navlinks" className={menuOpen ? "open" : "closed"}>
                     <Link to="/">Home</Link>
                     <Link to="/Alternative">Alternative</Link>
                     <Link to="/MindBody">Mind-Body</Link>
                     <Link to="/PhiloSpirit">Philosophy-Spiritual</Link>
-                </div>
+                    </div>
+                )}
+                <div id="menu-icon" onClick={() =>setMenuOpen(!menuOpen)}>
+                <div className="menu top-bar"></div>
+                <div className="menu mid-bar"></div>
+                <div className="menu bottom-bar"></div>
+            </div>
+            
+                   
+                
             </nav>
     )
 }
