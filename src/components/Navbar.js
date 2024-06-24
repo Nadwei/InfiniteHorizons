eccdf184179ccf821780dacd9c054ab099814815
NHammond <natehammond62@gmail.com>
2024-06-24 01:18:21 -0400

    subheadings and headings re-organized for 1st holistic wellness article

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 
 
 
 export default function Navbar(){
 const [menuOpen, setMenuOpen] = useState(false);
+const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
+
+useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
 
 
     return(
             <nav id="navbar">
                 
-                <h1 href="#" id="site-title"><a href="#">Infinite Horizons</a></h1>
+                <h1 id="site-title">
+                    
+                    <Link to="/">Infinite Horizons</Link>
+                </h1>
+                
                 
                     
                 {menuOpen && (
-                <div id="navlinks" className={menuOpen ? "open" : "closed"}>
+                <div id="navlinks" className={menuOpen  || isLargeScreen? "open" : "closed"}>
                     <Link to="/">Home</Link>
-                    <Link to="/Alternative">Alternative</Link>
-                    <Link to="/MindBody">Mind-Body</Link>
-                    <Link to="/PhiloSpirit">Philosophy-Spiritual</Link>
+                    <Link to="/Alternative">Tech/Alternative</Link>
+                    <Link to="/MindBody">Holistic Wellness</Link>
+                    <Link to="/PhiloSpirit">Personal Enlightenment</Link>
                     </div>
                 )}
                 <div id="menu-icon" onClick={() =>setMenuOpen(!menuOpen)}>
@@ -27,8 +47,6 @@ const [menuOpen, setMenuOpen] = useState(false);
                 <div className="menu bottom-bar"></div>
             </div>
             
-                   
-                
             </nav>
     )
 }
--- src/data/articleData.js
+++ src/data/articleData.js
@@ -2,9 +2,53 @@
 const articles = [
     {
       id: 1,
-      title: '',
-      date: '',
-      body: '',
+      title: 'A Philosophical Call to Action',
+      date: 'June 6, 2024',
+      paragraphs: {
+        paragraph_1:`In this time of rapid transformation, we stand on the brink of a new dawn. 
+        A global process of creation and unfoldment is unraveling before us, bringing both challenges and opportunities. 
+        As a global family, we face unprecedented and unparalleled times.
+        To navigate these complex dynamics, it has become increasingly critical to engender a response at the individual level. 
+        After all, of us holds the power to be a force for change, to stabilize and harmonize our world from the ground up.`,
+        paragraph_2: `The journey begins with the individual, the bedrock of society. 
+        From the individual, ripples of change extend to families, communities, and beyond. 
+        Society is nothing more than a collection of individuals, each contributing to the whole.
+        It is within this collective, decentalized power that we find the true potential for organic, self-organizing intelligence. 
+        By focusing on individual transformation, we can enact a pulse of self-correcting, self-stabilization in response to the acceleration of global change.`,
+        paragraph_3:`One of many approaches includes a flexible, human-centric template that transcends the full spectrum of diversity across the globe. 
+        An approach designed to foster self-actualization and self-realization in each person, promoting a grounded awareness and agentic unity of mind, body, and emotion.`,
+        paragraph_4:`Every human shares commonalities, such as the interplay of body, mind, and emotion. 
+        By maintaining constant contact with these dimensions of our lived experience, we can catalyze enduring self-transformations. 
+        This leads to a deeper liberating force of understanding within ourselves, between each other, and the world around us.`,
+        paragraph_4:`All external forms or "figures" arise from a context, much like plants emerge from the soil. 
+        By consciously influencing our context, we gain power over the various figures and forms which arise within us, both consciously and unconsciously. 
+        This awareness allows us to shape our destiny rather than being directed by it.`,
+        paragraph_5:`Our self-image, view of others, and worldview function like a compass, guiding us through life. 
+        Establishing and maintaining deep awareness of this relational complex is crucial for conscious self-creation and co-creation of our shared reality. 
+        As we become more aware, we can direct our lives with intention and purpose.`,
+        paragraph_6:`Through honest inquiry, self-transparency, and accountability, we can move towards greater functionality, wholeness, and self-realization. 
+        By purifying and reconciling our perceptions while recognizing the same in others, we foster a more harmonious existence.`,
+        paragraph_7:`Self-reflection and discovery reveal our core values and interests. 
+        These become critical tools for developing skills and abilities aligned with our natural composition. 
+        Over time, we can create a life that is fulfilling and aligned with our true selves.`,
+        paragraph_8:`A lifestyle and identity which nourish, support, and empower us emerges from this process. 
+        By establishing robust connections with ourselves, others, and the world, we become unshakable and nearly unstoppable.`,
+        paragraph_9:`In such chaotic and uncertain times, hardly anything could be a more invaluable tool than a template, vision, mission, and cause which earnestly seeks to extend the blessings of freedom, autonomy, self-realization, and self-actualization to every human being. 
+        It's in this vein we extend deep gratitude, appreciation, and encouragement to all in their own quest to catylize such a toolkit, map, and compass.
+        We encourage and invite all to see and feel deeply, and to keep the flame of hope and transformation alive, within and without.`
+      },
+      subHeadings: {
+        subheading_1:`A Call to Awakening`,
+        subheading_2:`The Power of the Individual`,
+        subheading_3:`A Human-Centric Approach`,
+        subheading_4:`Mind, Body, and Spirit`,
+        subheading_5:`Context and Emergence`,
+        subheading_6:`The Relational Complex`,
+        subheading_7:`From Part to Whole`,
+        subheading_8:`Value-Interest Alignment`,
+        subheading_9:`A Life of Meaning`,
+      },
+
       headerImage: {
         src: '',
         alt: '',
