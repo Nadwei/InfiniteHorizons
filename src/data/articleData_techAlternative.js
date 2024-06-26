00ed6720c1fe44c3130fa1072824d4c905ddd961
NHammond <natehammond62@gmail.com>
2024-06-25 20:17:56 -0400

    removed all filler text from other pages article data, ready to fill with new content

---
+++ src/data/articleData_techAlternative.js
@@ -0,0 +1,45 @@
+const articles = [
+    {
+      id: 1,
+      title: '',
+      date: '',
+      headerImage: {
+        src: "",
+        alt: '',
+      },
+      paragraph1: {
+        subheading_1: '',
+        subparagraph_1: ``,
+        subparagraph_2: ``,
+      },
+      
+      paragraph2: {
+        subheading_2: '',
+        subparagraph_1: ``,
+        subparagraph_2: ``,
+      },
+      bodyImage_1: { src: "", alt: '' },
+      paragraph3: {
+        subheading_3: '',
+        subparagraph_1: ``,
+        subparagraph_2: ``,
+      },
+      paragraph4: {
+        subheading_4: '',
+        subparagraph_1: ``,
+        subparagraph_2: ``,
+        subparagraph_3: ``,
+        subparagraph_4: ``,
+      },
+      bodyImage_2: { src: "", alt: '' },
+      paragraph5: {
+        subheading_5: '',
+        subparagraph_1: ``,
+        subparagraph_2: ``,
+        subparagraph_3: ``,
+      },
+    },
+  ];
+  
+  export default articles;
+  
