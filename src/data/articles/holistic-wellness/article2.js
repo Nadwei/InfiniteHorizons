d524ee2ed11499210a3f44d84819a485d8fd4175
NHammond <natehammond62@gmail.com>
2024-07-07 01:51:23 -0400

    added second holistic wellness article, still need images

---
+++ src/data/articles/holistic-wellness/article2.js
@@ -0,0 +1,112 @@
+import  mainImage from '../../../assets/images/articles/article3/article3-main.jpg';
+import bodyImage1 from '../../../assets/images/articles/article3/article3-1.jpg';
+import bodyImage2 from '../../../assets/images/articles/article3/article3-2.jpg';
+
+const holisticArticle = [
+  {
+    "id": 2,
+    "title": "Mind-Body-Emotion Complex and The Human Endeavor",
+    "date": "July 6, 2024",
+    "headerImage": {
+      "src": mainImage,
+      "alt": ""
+    },
+    "content": [
+      {
+        "type": "text",
+        "subheading": "The Mind, Body, and Emotions: A Complex Synergy",
+        "subparagraphs": [
+          "The mind, body, and emotions form a complex inherent to each human being, operating both as a synergistic whole and as independent parts, each with its own innate intelligence. Every individual possesses a unique composition and distribution of strengths and weaknesses in these areas, along with varying degrees of conscious, subconscious, and unconscious awareness of the processes occurring within each domain."
+        ]
+      },
+      {
+        "type": "text",
+        "subheading": "Evolving Domains and the Continuum of Transformation",
+        "subparagraphs": [
+          "While the overall size, range, depth, breadth, and scope of these conscious, subconscious, and unconscious domains remain unchanged, their contents and processes evolve over time. This creates a continuum of transformation where movement toward one end results in a narrowing and constriction of experience and agency, limiting the self’s ability to operate effectively within its environment. Conversely, movement toward the opposite end leads to an expansion and deepening of experience and agency, enhancing the self’s capability to interact with its surroundings.",
+        ]
+      },
+      {
+        "type": "image",
+        "src": bodyImage1,
+        "alt": "",
+        "className": "bodyImg1"
+      },
+      {
+        "type": "text",
+        "subheading": "Dimensions of the Multi-Faceted Self",
+        "subparagraphs": [
+          "This expansion/contraction continuum is just one dimension of the multi-faceted self as it relates to the present moment. Additionally, an individual’s potential exists along a perpendicular continuum, intersecting with and proportionate to the expansion/contraction dimension. This continuum of potential represents another dimension of the self, related to its embedding and actualization in time and space."
+        ]
+      },
+      {
+        "type": "text",
+        "subheading": "Manifesting Potential and Shifting Timelines",
+        "subparagraphs": [
+          "When an individual’s potential aligns with the expansion end of the continuum, it manifests as a widened aperture of experience, offering greater optionality, more choices, increased flexibility, and a higher degree of agency to resist current conditions. On this empowering end, individuals can choose from a broader array of alternate timelines, which they can shift into from their present moment in time and space.",
+          "Timelines are fluid, constantly shifting from moment to moment. They can be altered consciously through deliberate choice and conception of alternate possibilities. By envisioning a variety of options and exercising the power to accept, reject, or modify current conditions and impulses, individuals aim to align their anticipated outcomes with tangible results in the near future. These results represent the degree of change between the current moment and the conceived alternate timeline, guiding individuals to a new present condition that matches their initial vision."
+        ]
+      },
+      {
+        "type": "image",
+        "src": bodyImage2,
+        "alt": "",
+        "className": "bodyImg2"
+      },
+      {
+        "type": "text",
+        "subheading": "The Role of Logic in Manifestation",
+        "subparagraphs": [
+          "The logical faculty plays a crucial role in this process, as understanding cause and effect determines the accuracy of outcomes. Although countless variables in the natural world can alter the trajectory of events, cultivating logical comprehension and knowledge of these dynamics can reduce the variance between anticipated and actual outcomes. This ongoing practice enhances predictive power, enabling individuals to set more reliable expectations and achieve better results over time."
+        ]
+      },
+      {
+        "type": "text",
+        "subheading": "From Daily Practice to Mastery",
+        "subparagraphs": [
+          "Through this method, one can increase logical understanding of time, space, and cause/effect dynamics, allowing for the anticipation of likely outcomes and the alignment of efforts across various time frames. What begins as a daily practice can extend to weeks, months, years, and beyond, transforming the act of manifesting conceived mental models into tangible outcomes into an art form"]
+      },
+      {
+        "type": "text",
+        "subheading": "Manifestation: A Scientific and Creative Endeavor",
+        "subparagraphs": [
+          "Manifestation is not magic or pseudoscience; it is a scientific and creative application of universal laws and principles, leveraged daily by industries, leaders, businesses, and nations. While `thinking it into reality` and `willing it into being` have value, they represent only fragments of the full equation needed for reliable, empowering results."]
+      },
+      {
+        "type": "text",
+        "subheading": "From Daily Practice to Mastery",
+        "subparagraphs": [
+          "Through this method, one can increase logical understanding of time, space, and cause/effect dynamics, allowing for the anticipation of likely outcomes and the alignment of efforts across various time frames. What begins as a daily practice can extend to weeks, months, years, and beyond, transforming the act of manifesting conceived mental models into tangible outcomes into an art form."
+        ]
+      },
+      {
+        "type": "text",
+        "subheading": "Manifestation: A Scientific and Creative Endeavor",
+        "subparagraphs": [
+          "Manifestation is not magic or pseudoscience; it is a scientific and creative application of universal laws and principles, leveraged daily by industries, leaders, businesses, and nations. While `thinking it into reality`and `willing it into being` have value, they represent only fragments of the full equation needed for reliable, empowering results."
+        ]
+      },
+      {
+        "type": "text",
+        "subheading": "Essential Skills for Effective Manifestation",
+        "subparagraphs": [
+          "Effective manifestation involves clear planning, goal-setting, self-discipline, time management, measurement, self-accountability, and an ongoing commitment to understanding logical cause/effect and time/space dynamics. This includes recognizing the current state of things, the causal factors anchoring them, and how they transform over time when different forces are applied." ]
+      },
+      {
+        "type": "text",
+        "subheading": "The Scientific Pursuit of Predictive Power",
+        "subparagraphs": [
+          "Increasing predictive power is inherently a scientific endeavor, as science seeks to expand the individual’s ability to control outcomes through theory, equations, models, and logical rules. This pursuit extends the sphere of known and unknown phenomena, enhancing the agency, potential, and capacity of the human species as a whole." ]
+      },
+      {
+        "type": "text",
+        "subheading": "The Human Heritage of Knowledge and Creation",
+        "subparagraphs": [
+          "Humanity takes pride in its strides across all domains in this endeavor. The trials and triumphs, the horrors and beauty endured, overcome, or revealed through this effort, have brought us to a unique position today. With centuries of trial and error behind us, we continue the ancient tradition of knowledge-seeking, wisdom accumulation, and contributing to the collective evolutionary potential of our species.",
+          "This pursuit should be seen not as pseudoscience or secret knowledge but as a revered human heritage, an honor, a right, and an essential aspect of the human experience. To explore, discover, create, and share our contributions is to partake in the highest and greatest good we can conceive."]
+    },   
+    ]
+  }
+];
+
+export default holisticArticle;
